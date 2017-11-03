73e84951e25adf7054a75a198aec0efad7ad2ec2
Pixelastic <tim@pixelastic.com>
2017-11-03 18:26:53 +0100

    Move all code into one file

--- bin/markdown-preview.js
+++ bin/markdown-preview.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 /* eslint-disable import/no-commonjs */
-require('../index.js');
+const path = require('path');
+const fs = require('fs');
+const Markdown = require('markdown-it');
+const childProcess = require('child_process');
+
+const inputFile = path.resolve(process.argv[2]);
+const templateFile = path.join(__dirname, '../template.html');
+const template = fs.readFileSync(templateFile, 'utf-8');
+const input = fs.readFileSync(inputFile, 'utf-8');
+
+const html = new Markdown().render(input);
+
+const output = template.replace('{{MARKDOWN_CONTENT}}', html);
+
+fs.writeFileSync('/tmp/markdown-preview.html', output);
+
+childProcess.exec('google-chrome /tmp/markdown-preview.html');
+/* eslint-disable import/no-commonjs */
